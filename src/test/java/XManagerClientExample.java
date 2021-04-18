638d2ae375730292a25892dd3a2a68eb92cb552a
zeroleak <zeroleak@samourai.io>
2021-04-18 18:08:00 +0200

    add XManagerClientExample

---
+++ src/test/java/XManagerClientExample.java
@@ -0,0 +1,38 @@
+import com.samourai.http.client.IHttpClient;
+import com.samourai.xmanager.client.XManagerClient;
+import com.samourai.xmanager.protocol.XManagerEnv;
+import com.samourai.xmanager.protocol.XManagerService;
+import com.samourai.xmanager.protocol.rest.AddressIndexResponse;
+
+public class XManagerClientExample {
+  public void example() {
+    // configuration
+    boolean testnet = true;
+    boolean onion = false;
+    IHttpClient httpClient = null; // TODO provide AndroidHttpClient or CliHttpClient
+
+    // instanciation
+    XManagerClient xManagerClient = new XManagerClient(testnet, onion, httpClient);
+    XManagerEnv xManagerEnv = XManagerEnv.get(testnet);
+
+    // get address (or default when server unavailable)
+    String address = xManagerClient.getAddressOrDefault(XManagerService.RICOCHET);
+
+    // get address + index
+    AddressIndexResponse addressIndexResponse =
+        xManagerClient.getAddressIndexOrDefault(XManagerService.RICOCHET);
+    System.out.println(
+        "address=" + addressIndexResponse.address + ", index=" + addressIndexResponse.index);
+
+    // validate address + index
+    String addressToValidate = "...";
+    int indexToValidate = 0;
+    try {
+      boolean valid =
+          xManagerClient.verifyAddressIndexResponseOrException(
+              XManagerService.RICOCHET, addressToValidate, indexToValidate);
+    } catch (Exception e) {
+      // server not available
+    }
+  }
+}
